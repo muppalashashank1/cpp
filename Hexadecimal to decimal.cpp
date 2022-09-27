230544226dcdd6289c9fc99de493bb01718849f9
muppalashashank1 <113326617+muppalashashank1@users.noreply.github.com>
2022-09-27 10:01:28 +0530

    Add files via upload

---
+++ Hexadecimal to decimal.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+//convert hexadecimal to decimal
+int convert(char num[]) {
+   int len = strlen(num);
+   int base = 1;
+   int temp = 0;
+   for (int i=len-1; i>=0; i--) {
+      if (num[i]>='0' && num[i]<='9') {
+         temp += (num[i] - 48)*base;
+         base = base * 16;
+      }
+      else if (num[i]>='A' && num[i]<='F') {
+         temp += (num[i] - 55)*base;
+         base = base*16;
+      }
+   }
+   return temp;
+}
+int main() {
+   char num[] = "3F456A";
+   cout<<num<<" after converting to deciaml becomes : "<<convert(num)<<endl;
+   return 0;
+}
